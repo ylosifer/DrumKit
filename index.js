0a914d65ab6266f14ec118506bda7f94f03def4b
Shourya Pachauri <92849643+ylosifer@users.noreply.github.com>
2023-12-19 23:03:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,76 @@
   var noOfDrums = document.querySelectorAll(".drum").length;
 
   for (var i = 0; i < noOfDrums; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("keydown", function (e) {
+    document.querySelectorAll(".drum")[i].addEventListener("keydown", function (event) {
       var buttonInnerHTML = this.event;
-      switch (e.code) {
-        case "KeyW":
-          var audio = new Audio("tom-1.mp3");
+      switch (event.key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
           audio.play();
           break;
-        case "KeyA":
-          var tom2 = new Audio('tom-2.mp3');
+        case "a":
+          var tom2 = new Audio('sounds/tom-2.mp3');
           tom2.play();
           break;
-        case "KeyS":
-          var tom3 = new Audio('tom-3.mp3');
+        case "s":
+          var tom3 = new Audio('sounds/tom-3.mp3');
           tom3.play();
           break;
-        case "KeyD":
-          var tom4 = new Audio('tom-4.mp3');
+        case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
           tom4.play();
           break;
-        case "KeyJ":
-          var snare = new Audio('snare.mp3');
+        case "j":
+          var snare = new Audio('sounds/snare.mp3');
           snare.play();
           break;
-        case "KeyK":
-          var crash = new Audio('crash.mp3');
+        case "k":
+          var crash = new Audio('sounds/crash.mp3');
           crash.play();
           break;
-        case "KeyL":
-          var kick = new Audio('kick-bass.mp3');
+        case "l":
+          var kick = new Audio('sounds/kick-bass.mp3');
+          kick.play();
+          break;
+        default:
+          console.log(buttonInnerHTML);
+      }
+    });
+  }
+
+
+  
+
+  for (var i = 0; i < noOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+      var buttonInnerHTML = this.innerHTML;
+      switch (buttonInnerHTML) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        case "a":
+          var tom2 = new Audio('sounds/tom-2.mp3');
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio('sounds/tom-3.mp3');
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
+          tom4.play();
+          break;
+        case "j":
+          var snare = new Audio('sounds/snare.mp3');
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio('sounds/crash.mp3');
+          crash.play();
+          break;
+        case "l":
+          var kick = new Audio('sounds/kick-bass.mp3');
           kick.play();
           break;
         default:
